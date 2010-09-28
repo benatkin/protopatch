c1f949f29b34605e71b364b7c2fa9d26d306e105
ben@benatkin.com <ben@benatkin.com>
2010-09-27 23:39:38 -0600

    added ">" example

--- lib/fid.rb
+++ lib/fid.rb
@@ -2,8 +2,14 @@ module Fid
   def self.diff(doc1, doc2)
     _diff = {}
     ({}.to_set + doc1.keys + doc2.keys).each do |k|
-      if doc1.include?(k)
+      if doc1.include?(k) and doc2.include?(k)
+        unless doc1[k] == doc2[k]
+          _diff[k] = {'<' => doc1[k], '>' => doc2[k]}
+        end
+      elsif doc1.include?(k)
         _diff[k] = {'<' => doc1[k]}
+      elsif doc2.include?(k)
+        _diff[k] = {'>' => doc2[k]}
       end
     end
     _diff
--- spec/fid_spec.rb
+++ spec/fid_spec.rb
@@ -8,5 +8,9 @@ describe Fid, "#diff" do
   it 'returns "<" for item not in 2nd object' do
     Fid.diff({'apples' => 1}, {}).should == {'apples' => {'<' => 1}}
   end
+
+  it 'returns ">" for item not in 1st object' do
+    Fid.diff({}, {'bananas' => 3}).should == {'bananas' => {'>' => 3}}
+  end
 end
 
