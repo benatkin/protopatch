32fb7015be09b1a51a978126c43db8cbd1630b6a
ben@benatkin.com <ben@benatkin.com>
2010-09-27 23:52:02 -0600

    added remaining flat examples

--- spec/fid_spec.rb
+++ spec/fid_spec.rb
@@ -12,5 +12,38 @@ describe Fid, "#diff" do
   it 'returns ">" for item not in 1st object' do
     Fid.diff({}, {'bananas' => 3}).should == {'bananas' => {'>' => 3}}
   end
+
+  it 'returns "<" and "> for changed item' do
+    Fid.diff({'bananas' => 5}, {'bananas' => 3})
+       .should == {'bananas' => {'<' => 5, '>' => 3}}
+  end
+
+  it 'omits unchanged item' do
+    Fid.diff({'bananas' => 5}, {'bananas' => 5}).should == {}
+  end
+
+  it 'correctly returns diff from README' do
+    Fid.diff({
+               "pears"   => 1,
+               "apples"  => {"red" => 2, "green" => 1},
+               "bananas" => 5,
+               "mangos"  => 2
+             },
+             {
+               "apples"  => {"golden" => 1},
+               "bananas" => 3,
+               "oranges" => 6,
+               "mangos"  => 2
+             })
+       .should == {
+                    "apples"  => {
+                                   ">" => {"golden" => 1},
+                                   "<" => {"red" => 2, "green" => 1}
+                                 },
+                    "bananas" => {">" => 3, "<" => 5},
+                    "oranges" => {">" => 6},
+                    "pears"   => {"<" => 1}
+                  }
+  end
 end
 
