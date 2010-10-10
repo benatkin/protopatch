865f0e6f0b2ea79b471f8c23af3cf8e9c7ed45a5
ben@benatkin.com <ben@benatkin.com>
2010-10-10 14:13:14 -0600

    refactor ruby code to use instance

--- lib/fid.rb
+++ lib/fid.rb
@@ -1,46 +1,17 @@
 module Fid
-  def self.diff(a, b)
-    if a == b
-      nil
-    elsif (a.keys & b.keys).size > 0
-      p = {}
-      (a.keys | b.keys).each do |k|
-        if a.include?(k) and b.include?(k)
-          unless a[k] == b[k]
-            p[k] = {'-' => a[k], '+' => b[k]}
-          end
-        elsif a.include?(k)
-          p[k] = {'-' => a[k]}
-        elsif b.include?(k)
-          p[k] = {'+' => b[k]}
-        end
-      end
-      p
-    else
-      p = {'-' => a, '+' => b}
+  class << self
+    attr_writer   :default_differ
+
+  private
+    def method_missing(name, *args, &block)
+      default_differ.send(name, *args, &block)
     end
   end
-
-  def self.patch(a, p)
-    case p
-      when Hash
-        if p.include?('-') and p.include?('+')
-          p['+']
-        else
-          patched = a.clone
-          p.each do |k, v|
-            if v.include?('-') and v.include?('+')
-              patched[k] = v['+']
-            elsif v.include?('-')
-              patched.delete(k)
-            elsif v.include?('+')
-              patched[k] = v['+']
-            end
-          end
-          patched
-        end
-      when nil
-        a
-    end
+  
+  def self.default_differ
+    @default_differ || Differ.new
   end
 end
+
+require 'fid/differ'
+
